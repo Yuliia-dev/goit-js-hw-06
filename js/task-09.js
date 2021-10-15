8b7b967b63c6527af935e89f8fc9f04e5c27d2d9
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2021-10-15 21:04:34 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  
 }
+
+
+const colorNameEl = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+
+btnChangeColor.addEventListener('click', onChangeColor);
+btnChangeColor.addEventListener('click', getRandomHexColor);
+
+function onChangeColor(event) {
+  const bodyEl = document.querySelector('body');
+  bodyEl.classList.add('color');
+  const colorBody = document.querySelector('.color');
+
+  if (event) {  
+    colorBody.style.backgroundColor = getRandomHexColor();
+  }
+  colorNameEl.textContent = getRandomHexColor();
+  
+}
+
